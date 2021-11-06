82bba9f0937f92168b6649ab3b40509662c61e0c
Claudia Font <claudiafontmarques@gmail.com>
2021-11-06 13:16:21 +0100

    added actionTypes to actionTypes file

---
+++ src/redux/actions/actionTypes.js
@@ -0,0 +1,8 @@
+const actionTypes = {
+  loadRobots: "LOAD_ROBOTS",
+  createRobot: "CREATE_ROBOT",
+  updateRobot: "UPDATE_ROBOT",
+  deleteRobot: "DELETE_ROBOT",
+};
+
+export default actionTypes;
