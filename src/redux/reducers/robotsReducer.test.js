d492cdba7882b99b3e6b6bdd89e300972812ab15
Claudia Font <claudiafontmarques@gmail.com>
2021-11-06 18:00:54 +0100

    Tested default case of robotsReducers

--- src/redux/reducers/robotsReducer.test.js
+++ src/redux/reducers/robotsReducer.test.js
@@ -16,4 +16,15 @@ describe("Given a robotsReducer reducer", () => {
       expect(newRobotsList).toEqual(robotsList);
     });
   });
+  describe("when it receives a robots list with five robots an unknown action", () => {
+    test("Then it should return the receives robots list", () => {
+      const initialRobots = getRandomRobots(5);
+      const action = {
+        type: actionTypes.FakeAction,
+      };
+      const newRobotsList = robotsReducer(initialRobots, action);
+
+      expect(newRobotsList).toEqual(initialRobots);
+    });
+  });
 });
