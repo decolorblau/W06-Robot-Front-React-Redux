7bccaa45a6d0acc8cf1ef0f386347818fac78c87
Claudia Font <claudiafontmarques@gmail.com>
2021-11-07 22:07:03 +0100

    Added css to Robot component

--- src/components/RobotsList/RobotsList.js
+++ src/components/RobotsList/RobotsList.js
@@ -14,7 +14,10 @@ const RobotsList = () => {
       <h2>List of Robots</h2>
       <ul className="list-unstyled row">
         {robots.map((robot) => (
-          <li className="col-4" key={robot._id}>
+          <li
+            className="col-lg-4 col-md-6 col-sm-12 mb-2 d-flex align-items-stretch"
+            key={robot._id}
+          >
             <Robot robot={robot} />
           </li>
         ))}
