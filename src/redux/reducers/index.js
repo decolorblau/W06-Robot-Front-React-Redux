2d93638d3d4b775d2e4bbd3754d18258a4a4eb14
Claudia Font <claudiafontmarques@gmail.com>
2021-11-08 19:08:02 +0100

    userReducer to root reducer

--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import robotsReducer from "./robotsReducer";
+import userReducer from "./userReducer";
 
 const rootReducer = combineReducers({
   robots: robotsReducer,
+  user: userReducer,
 });
 
 export default rootReducer;
