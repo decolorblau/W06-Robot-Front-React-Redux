08c8834a83a3d56cc84ab24780fe6c033162793e
Claudia Font <claudiafontmarques@gmail.com>
2021-11-08 19:06:25 +0100

    corrected ortografy errors

--- src/redux/reducers/userReducer.js
+++ src/redux/reducers/userReducer.js
@@ -2,7 +2,7 @@ import actionTypes from "../actions/actionTypes";
 
 const userReducer = (
   user = {
-    isAuthentidated: false,
+    isAuthenticated: false,
     user: {},
   },
   action
@@ -12,13 +12,13 @@ const userReducer = (
   switch (action.type) {
     case actionTypes.loginUser:
       newUser = {
-        isAuthentidated: true,
+        isAuthenticated: true,
         user: action.user,
       };
       break;
     case actionTypes.logoutUser:
       newUser = {
-        isAuthentidated: false,
+        isAuthenticated: false,
         user: {},
       };
       break;
