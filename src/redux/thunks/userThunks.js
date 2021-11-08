2b75be6a4fcb14e5bfda58f419a558fdc74f4618
Claudia Font <claudiafontmarques@gmail.com>
2021-11-08 19:20:42 +0100

    added user thunk

---
+++ src/redux/thunks/userThunks.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import { userLoginAction } from "../actions/actionCreator";
+import jwtDecode from "jwt-decode";
+
+const userLocal = process.env.REACT_APP_LOCALSTORAGE;
+
+const urlApi = "https://robots-decolorblau.herokuapp.com/";
+
+export const loginUserThunk = (user) => async (dispatch) => {
+  const response = await axios.post(urlApi + "users/login", user);
+  if (response.status === 200) {
+    const token = response.data.token;
+    const user = jwtDecode(token);
+    dispatch(userLoginAction(user));
+
+    localStorage.setItem(userLocal, JSON.stringify({ token: token }));
+  }
+};
